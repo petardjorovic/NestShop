de44ac545857c306b79e90760024fd2ed1dbe290
petardjorovic <perodjorovic@gmail.com>
2026-06-29 23:41:33 +0200

    db tables

--- src/administrator/administrator.controller.ts
+++ src/administrator/administrator.controller.ts
@@ -1,17 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 
 import { Serialize } from 'src/decorators/serialize.decorators';
 import { AdministratorDto } from './dtos/administrator.dto';
 import { AdministratorService } from './administrator.service';
-import { User } from 'src/generated/prisma/client';
+import { Administrator } from 'src/generated/prisma/client';
+import { ApiResponse } from 'src/misc/api.response.class';
 
-@Serialize(AdministratorDto)
+// @Serialize(AdministratorDto)
 @Controller('api/administrator')
 export class AdministratorController {
   constructor(private readonly administratorService: AdministratorService) {}
 
   @Get()
-  findAll(): Promise<User[]> {
+  findAllAdmins(): Promise<Administrator[]> {
     return this.administratorService.findAll();
+  }
+
+  @Get(':id')
+  findAdminById(
+    @Param('id', ParseIntPipe) id: number,
+  ): Promise<Administrator | ApiResponse> {
+    return this.administratorService.findById(id);
   }
 }
--- src/administrator/administrator.service.ts
+++ src/administrator/administrator.service.ts
@@ -1,12 +1,25 @@
 import { Injectable } from '@nestjs/common';
-import { User } from 'src/generated/prisma/client';
+import { Administrator } from 'src/generated/prisma/client';
+import { ApiResponse } from 'src/misc/api.response.class';
 import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
 export class AdministratorService {
   constructor(private readonly prisma: PrismaService) {}
 
-  findAll(): Promise<User[]> {
-    return this.prisma.user.findMany();
+  findAll(): Promise<Administrator[]> {
+    return this.prisma.administrator.findMany();
+  }
+
+  async findById(
+    administratorId: number,
+  ): Promise<Administrator | ApiResponse> {
+    const admin = await this.prisma.administrator.findFirst({
+      where: { administratorId },
+    });
+    if (!admin) {
+      return new ApiResponse('Not Found', 10001);
+    }
+    return admin;
   }
 }
